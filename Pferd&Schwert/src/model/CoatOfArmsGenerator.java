42b810c51c4c04690beb220869b7e72637838e17
markus <markusdresel@gmx.de>
2017-06-19 19:32:40 +0200

    Mehr Farben und leere Variationen.

--- Pferd&Schwert/src/model/CoatOfArmsGenerator.java
+++ Pferd&Schwert/src/model/CoatOfArmsGenerator.java
@@ -14,9 +14,10 @@ public class CoatOfArmsGenerator {
 	public String path;
 	public String[] symbolNames = {"Pferd2b", "Stern2b", "Stern1b", "Rad1b"};
 	public String[] overlayNames = {"Halb", "Halb2", "Viertel2", "Viertel", "Horizont", "Horizont2", "Diagonal", "Diagonal2", "Dreieck", "Dreieck2", "QuerViertel", "QuerViertel2"};
-	public Color[] farben = {new Color(240,240,240), new Color(30,30,30), new Color(150,0,0), new Color(0,150,0), new Color(0,0,150)};
-	public BufferedImage[] symbols = new BufferedImage[symbolNames.length];
-	public BufferedImage[] shieldOverlays = new BufferedImage[overlayNames.length];
+	public Color[] farben = {new Color(240,240,240), new Color(30,30,30), new Color(160,0,0), new Color(30,150,30), new Color(0,0,160), new Color(200,200,0), new Color(90,0,0), 
+							 new Color(100,100,255), new Color(0,80,0), new Color(100,100,100), new Color(90,10,90), new Color(200,100,0), new Color(10,90,90), new Color(0,0,80)};
+	public BufferedImage[] symbols = new BufferedImage[symbolNames.length+1];
+	public BufferedImage[] shieldOverlays = new BufferedImage[overlayNames.length+1];
     private BufferedImage schildschatten;
     private BufferedImage schild;
     // Konstruktor
@@ -39,37 +40,43 @@ public class CoatOfArmsGenerator {
     // neues Wappen generieren
     public BufferedImage GenerateCoatOfArms() throws IOException{
     	
+        // Wappen initialisieren
+        BufferedImage combined = new BufferedImage(schildschatten.getWidth(), schildschatten.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics g = combined.getGraphics();
+    	
     	// Schild einfaerben
     	Color schildFarbe = farben[r.nextInt(farben.length)];
         schild = dye(schild, new Color(schildFarbe.getRed(), schildFarbe.getGreen(), schildFarbe.getBlue(), 255));
+        g.drawImage(schild, 0, 0, null);
     	
         // Overlay auswuerfeln
         BufferedImage schildOverlay = shieldOverlays[r.nextInt(shieldOverlays.length)];
+    	Color overlayFarbe = farben[r.nextInt(farben.length)];
+        if(schildOverlay != null) {
         
-        // Overlay einfaerben
-        Color overlayFarbe = farben[r.nextInt(farben.length)];
-        while(overlayFarbe.equals(schildFarbe)) {
-        	overlayFarbe = farben[r.nextInt(farben.length)];
+        	// Overlay einfaerben
+        	while(overlayFarbe.equals(schildFarbe)) {
+        		overlayFarbe = farben[r.nextInt(farben.length)];
+        	}
+        	schildOverlay = dye(schildOverlay, new Color(overlayFarbe.getRed(), overlayFarbe.getGreen(), overlayFarbe.getBlue(), 255));
+        	g.drawImage(schildOverlay, 0, 0, null);
+        
         }
-        schildOverlay = dye(schildOverlay, new Color(overlayFarbe.getRed(), overlayFarbe.getGreen(), overlayFarbe.getBlue(), 255));
         
     	// Zeichen auswuerfeln
         BufferedImage zeichen = symbols[r.nextInt(symbols.length)];
-        
-        // Zeichen einfaerben
         Color zeichenFarbe = farben[r.nextInt(farben.length)];
-        while(zeichenFarbe.equals(schildFarbe) || zeichenFarbe.equals(overlayFarbe)) {
-        	zeichenFarbe = farben[r.nextInt(farben.length)];
-        }
-        zeichen = dye(zeichen, new Color(zeichenFarbe.getRed(), zeichenFarbe.getGreen(), zeichenFarbe.getBlue(), 120));
+        if(zeichen != null) {
         
-        // Alles zusammensetzen
-        BufferedImage combined = new BufferedImage(schildschatten.getWidth(), schildschatten.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        Graphics g = combined.getGraphics();
-        g.drawImage(schild, 0, 0, null);
-        g.drawImage(schildOverlay, 0, 0, null);
-        g.drawImage(zeichen, 145, 145, null);
-      //g.drawImage(image3, 120, 120, null);
+        	// Zeichen einfaerben
+        	while(zeichenFarbe.equals(schildFarbe) || zeichenFarbe.equals(overlayFarbe)) {
+        		zeichenFarbe = farben[r.nextInt(farben.length)];
+        	}
+        	zeichen = dye(zeichen, new Color(zeichenFarbe.getRed(), zeichenFarbe.getGreen(), zeichenFarbe.getBlue(), 120));
+        	g.drawImage(zeichen, 150, 195, null);
+        }
+        
+        // Schildschatten hinzufügen
         g.drawImage(schildschatten, 0, 0, null);
         
 		return combined;
