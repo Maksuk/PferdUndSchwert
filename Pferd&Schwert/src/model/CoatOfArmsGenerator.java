f59900ed12bec2237eae4f9565a41f7d3077ac70
Brigor <patrick.dresel@gmx.de>
2017-06-22 13:57:19 +0200

    Erste Muster für Wappen hinzugefügt.

--- Pferd&Schwert/src/model/CoatOfArmsGenerator.java
+++ Pferd&Schwert/src/model/CoatOfArmsGenerator.java
@@ -12,12 +12,14 @@ public class CoatOfArmsGenerator {
 	
 	public Random r = new Random();
 	public String path;
-	public String[] symbolNames = {"Pferd2b", "Stern2b", "Stern1b", "Rad1b"};
+	public String[] symbolNames = {"Pferd2b", "Stern2b", "Stern1b", "Rad1b", "Drache1", "Gral1", "Lilie1"}; 
 	public String[] overlayNames = {"Halb", "Halb2", "Viertel2", "Viertel", "Horizont", "Horizont2", "Diagonal", "Diagonal2", "Dreieck", "Dreieck2", "QuerViertel", "QuerViertel2"};
+	public String[] patternNames = {"StreifenVertikal", "StreifenHorizontal"};
 	public Color[] farben = {new Color(240,240,240), new Color(30,30,30), new Color(160,0,0), new Color(30,150,30), new Color(0,0,160), new Color(200,200,0), new Color(90,0,0), 
 							 new Color(100,100,255), new Color(0,80,0), new Color(100,100,100), new Color(90,10,90), new Color(200,100,0), new Color(10,90,90), new Color(0,0,80)};
 	public BufferedImage[] symbols = new BufferedImage[symbolNames.length+1];
 	public BufferedImage[] shieldOverlays = new BufferedImage[overlayNames.length+1];
+	public BufferedImage[] patterns = new BufferedImage[patternNames.length];
     private BufferedImage schildschatten;
     private BufferedImage schild;
     // Konstruktor
@@ -32,6 +34,9 @@ public class CoatOfArmsGenerator {
     	}
     	for(int i=0; i<overlayNames.length; i++) {
     		shieldOverlays[i] = ImageIO.read(new File(path + "Schild" + overlayNames[i]+ ".png"));
+    	}
+    	for(int i=0; i<patternNames.length; i++) {
+    		patterns[i] = ImageIO.read(new File(path + patternNames[i]+ ".png"));
     	}
     	schild = ImageIO.read(new File(path + "Schild2.png"));
     	schildschatten = ImageIO.read(new File(path + "SchildS2.png"));
@@ -49,6 +54,21 @@ public class CoatOfArmsGenerator {
         schild = dye(schild, new Color(schildFarbe.getRed(), schildFarbe.getGreen(), schildFarbe.getBlue(), 255));
         g.drawImage(schild, 0, 0, null);
     	
+        // Muster mit bestimmter Wahrscheinlichkeit einfuegen
+        Color musterFarbe = farben[r.nextInt(farben.length)];
+        if(r.nextInt(100) < 50) {
+        	
+        	// Muster auswuerfeln
+        	BufferedImage muster = patterns[r.nextInt(patterns.length)];
+        	
+        	// Muster einfaerben
+        	while(musterFarbe.equals(schildFarbe)) {
+        		musterFarbe = farben[r.nextInt(farben.length)];
+        	}
+        	muster = dye(muster, new Color(musterFarbe.getRed(), musterFarbe.getGreen(), musterFarbe.getBlue(), 255));
+        	g.drawImage(muster, 0, 0, null);
+        }
+        
         // Overlay auswuerfeln
         BufferedImage schildOverlay = shieldOverlays[r.nextInt(shieldOverlays.length)];
     	Color overlayFarbe = farben[r.nextInt(farben.length)];
@@ -69,14 +89,14 @@ public class CoatOfArmsGenerator {
         if(zeichen != null) {
         
         	// Zeichen einfaerben
-        	while(zeichenFarbe.equals(schildFarbe) || zeichenFarbe.equals(overlayFarbe)) {
+        	while(zeichenFarbe.equals(schildFarbe) || zeichenFarbe.equals(overlayFarbe) || zeichenFarbe.equals(musterFarbe)) {
         		zeichenFarbe = farben[r.nextInt(farben.length)];
         	}
         	zeichen = dye(zeichen, new Color(zeichenFarbe.getRed(), zeichenFarbe.getGreen(), zeichenFarbe.getBlue(), 120));
-        	g.drawImage(zeichen, 150, 195, null);
+        	g.drawImage(zeichen, 117, 163, null);
         }
         
-        // Schildschatten hinzufügen
+        // Schildschatten hinzufuegen
         g.drawImage(schildschatten, 0, 0, null);
         
 		return combined;
